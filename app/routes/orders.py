e5176367b50b718a2d05ab2c2a5bf90caeccbc72
Christopher Hauser <chauser6@gmail.com>
2021-12-29 17:46:14 -0500

    still working

--- app/routes/orders.py
+++ app/routes/orders.py
@@ -1,7 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
+import psycopg2
 from flask_login import login_required
 
 bp = Blueprint("orders", __name__, url_prefix="")
+
+CONNECTION_PARAMETERS = {
+    "dbname": "order_up_dev",
+    "user": "order_up",
+    "password": "9uCxydbt"
+}
 
 @bp.route("/")
 @login_required
@@ -9,3 +16,39 @@ def index():
     return render_template("orders.html")
 
 
+@bp.route("/assign_table")
+@login_required
+def assign_table(table_id, employee_id):
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                """
+                INSERT INTO orders (table_id, employee_id)
+                VALUES (%(table_id)s, %(employee_id)s)
+                """,
+                {
+                    "table_id": table_id,
+                    "employee_id": employee_id
+                }
+            )
+        return redirect('/')
+
+@bp.route("/close_table")
+@login_required
+def close_table(order_id):
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+        with conn.cursor() as curs:
+            curs.execute(
+                """
+                SELECT id FROM orders
+                WHERE (%(order_id)s == id)
+                """,
+                {
+                    'order_id': order_id
+                }
+            )
+
+@bp.route("/add_to_order")
+@login_required
+def add_to_order():
+    pass
